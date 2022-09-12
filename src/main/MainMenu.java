211b5e9d5c8866045806acf1f910e8b3109cbcfb
Henrique de Oliveira Ramos <henriqueramos.qs@gmail.com>
2022-09-12 00:23:05 -0300

    feat: menu inicial

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -3,55 +3,68 @@ package main;
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 import javax.swing.JButton;
 import javax.imageio.ImageIO;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
 
 public class MainMenu extends JFrame {
-    private  JButton startButton; 
-    private  JButton rankingButton;
+    private JButton startButton;
+    private JButton rankingButton;
 
-    public MainMenu(){
+    public MainMenu() {
         super("Testing Buttons");
         setLayout(new BorderLayout());
+        Color backgroundColor = new Color(254, 207, 177);
+        Color detailColor = new Color(86, 39, 9);
 
         try {
             setContentPane(new JLabel(new ImageIcon(ImageIO.read(new File("src/assets/menu_background.png")))));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        setLayout(new FlowLayout());
-        startButton = new JButton("START"); 
-        add(startButton); 
+        setLayout(new FlowLayout(FlowLayout.CENTER, 0, 160));
 
-        rankingButton = new JButton("RANKING"); 
-        add(rankingButton); 
+        JPanel auxPanel = new JPanel();
+        auxPanel.setLayout(new BoxLayout(auxPanel, BoxLayout.PAGE_AXIS));
+        auxPanel.setBackground(backgroundColor);
+
+        startButton = new JButton("  START  ");
+        // startButton.setBorder(new LineBorder(detailColor,1,true));
+        startButton.setForeground(detailColor);
+
+        auxPanel.add(startButton);
+
+        rankingButton = new JButton("RANKING");
+        // rankingButton.setBorder(new LineBorder(detailColor,1,true));
+        // rankingButton.setBackground(backgroundColor);
+        rankingButton.setForeground(detailColor);
+        auxPanel.add(rankingButton);
 
         StartHandler sh = new StartHandler();
         startButton.addActionListener(sh);
 
         RankingHandler rh = new RankingHandler();
         rankingButton.addActionListener(rh);
-        // //refresh )
-        // setSize(255,239);
-        // setSize(256,240);
 
-     }
+        add(auxPanel);
+
+    }
 
     private class StartHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent event) {
             App game = new App();
-		    game.start();
+            game.start();
             dispose();
         }
     }
@@ -59,7 +72,8 @@ public class MainMenu extends JFrame {
     private class RankingHandler implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent event) {
-            //faz alguma coisa   
+            // faz alguma coisa
         }
     }
-} 
+
+}
